33f45f8a04934c22c61b1bd792383fd2653042cd
Michael Chris Trimbach <trimbamc@miamioh.edu>
2021-02-25 22:13:36 -0500

    Implemented Cursor and moving Molecules
    Added a cursor image to the canvas that follows the player's mouse movements, and allowed the player to pickup molecules they are hovering over and drop them by using the left mouse button.

--- Project_Robo/Assets/Scripts/PuzzleCode/MoleculeNode.cs
+++ Project_Robo/Assets/Scripts/PuzzleCode/MoleculeNode.cs
@@ -2,14 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
+using Rewired;
 
 public class MoleculeNode : MonoBehaviour
-{ 
+{
+    #region Rewired Stuff
+    //Here, we establish what a name that we will use instead of "Input" 
+    private Rewired.Player player;
+    //The playerID lables which player you are, 0=P1, 1=P2, and so on.
+    public int playerId = 0;
+    //Stuff for rumble support
+    int motorIndex0 = 0;
+    int motorIndex1 = 1;
+    #endregion
+
+    #region Awake
+    //Awake function is code that is executed before the Start or OnEnable functions. 
+    private void Awake()
+    {
+
+        //THIS LINE IS CRUCIAL! IF IT IS NOT IN THE SCRIPT REWIRD WONT READ THE INPUTS FROM THE PROPER CONTROL INPUT!
+        player = Rewired.ReInput.players.GetPlayer(playerId);
+    }
+    #endregion
 
     public bool moving = false;
+    [SerializeField] private bool canMove;
     [SerializeField] public Color nodeColor = Color.red;
     [SerializeField] public string nodeType = "basic";
     [SerializeField] public Image thisNode;
+    [SerializeField] public Image cursor;
+    public RectTransform rect = null;
+    public CursorFollow follow = null;
+    public Collider2D collide = null;
 
     // Start is called before the first frame update
     void Start()
@@ -18,11 +43,47 @@ public class MoleculeNode : MonoBehaviour
         {
             thisNode.color = nodeColor;
         }
+
+        if (cursor != null)
+            follow = cursor.GetComponent<CursorFollow>();
+
+        rect = GetComponent<RectTransform>();
+        collide = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (canMove && collide.IsTouching(cursor.GetComponent<Collider2D>()))
+        {
+            if (!follow.carrying)
+            {
+                if (player.GetButtonDown("Action")) {
+
+                    moving = true;
+                    follow.carrying = true;
+                }
+            }
+            else
+            {
+                if (player.GetButtonDown("Action"))
+                {
+
+                    moving = false;
+                    follow.carrying = false;
+                }
+            }
+        }
+
+
+        HandleMotion();
+    }
+
+    void HandleMotion()
+    {
+        if (moving)
+        {
+            rect.transform.position = cursor.rectTransform.position;
+        }
     }
 }
--- Project_Robo/Assets/Scripts/PuzzleCode/PuzzleUI.cs
+++ Project_Robo/Assets/Scripts/PuzzleCode/PuzzleUI.cs
@@ -5,7 +5,27 @@ using UnityEngine.UI;
 using Rewired;
 
 public class PuzzleUI : MonoBehaviour
-{ 
+{
+    #region Rewired Stuff
+    //Here, we establish what a name that we will use instead of "Input" 
+    private Rewired.Player player;
+    //The playerID lables which player you are, 0=P1, 1=P2, and so on.
+    public int playerId = 0;
+    //Stuff for rumble support
+    int motorIndex0 = 0;
+    int motorIndex1 = 1;
+    #endregion
+
+    #region Awake
+    //Awake function is code that is executed before the Start or OnEnable functions. 
+    private void Awake()
+    {
+
+        //THIS LINE IS CRUCIAL! IF IT IS NOT IN THE SCRIPT REWIRD WONT READ THE INPUTS FROM THE PROPER CONTROL INPUT!
+        player = Rewired.ReInput.players.GetPlayer(playerId);
+    }
+    #endregion
+
     [SerializeField] public GameObject puzzle1;
     private ArrayList puzzle1Nodes;
 
@@ -19,7 +39,10 @@ public class PuzzleUI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        /*if (new AxisCoordinateMode)
+        {
+
+        }*/
     }
 
     public void togglePuzzle1()

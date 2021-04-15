7bf53e948588986babac025b1cd472fbc8560c57
cesheard <cesheard@hotmail.com>
2021-04-15 19:58:29 -0400

    Implemented the FirstDoor, Other Door Stuff
    Made the isOpening bool, set up the conditions in the animator controller 'Door Animator', put the door model in the scene, added the DoorBehavior script to the door, put the door in the door list in the LevelMananger.
    Also set it so that the sleeping bay doors don't loop after opening.

--- Project_Robo/Assets/Scripts/DoorBehavior.cs
+++ Project_Robo/Assets/Scripts/DoorBehavior.cs
@@ -22,13 +22,14 @@ public class DoorBehavior : MonoBehaviour
     public void Open() { //opens the door when called
         isOpen = true;
         Debug.Log("The Door is now OPEN!!!!!!");
-        //anim.SetBool("isOpening",true);
-        
+        anim.SetBool("isOpening",true);
+        GameObject.Find("FirstDoor").GetComponent<BoxCollider>().enabled = false;
 
     }
     public void Close(){ //closes the door when called
         isOpen = false;
         Debug.Log("The Door is now Closed :(");
-        //anim.SetBool("isClosing", true);
+        anim.SetBool("isOpening", true);
+        GameObject.Find("FirstDoor").GetComponent<BoxCollider>().enabled = false;
     }
 }
